ea0827f9c3fc2a23c01a0d5f528be8b702fa78cf
Ravi <nalavde@gmail.com>
2019-07-12 20:42:06 +0530

    Added characterization test

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -5,11 +5,29 @@ import org.junit.Test;
 
 public class CustomerTest {
     @Test
-    public void test() {
+    public void shouldValidStatement() {
+        Movie movie1 = new Movie("test1", Movie.CHILDRENS);
+        Movie movie2 = new Movie("test2", Movie.NEW_RELEASE);
+        Movie movie3 = new Movie("test3", Movie.REGULAR);
 
-        int expected = 3;
+        Rental rental1 = new Rental(movie1, 2);
+        Rental rental2 = new Rental(movie1, 3);
+        Rental rental3 = new Rental(movie1, 4);
 
-        Assert.assertEquals(expected, 1 + 2);
+        Customer customer = new Customer("Sample_Customer");
+        customer.addRental(rental1);
+        customer.addRental(rental2);
+        customer.addRental(rental3);
+
+        String expected = "Rental Record for Sample_Customer\n" +
+                "\ttest1\t1.5\n" +
+                "\ttest1\t1.5\n" +
+                "\ttest1\t3.0\n" +
+                "Amount owed is 6.0\n" +
+                "You earned 3 frequent renter points";
+
+        Assert.assertEquals(expected, customer.statement());
+
     }
 
 }
